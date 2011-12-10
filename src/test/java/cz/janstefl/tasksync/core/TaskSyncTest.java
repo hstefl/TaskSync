9520a67d4254275f492e024cc9b84d3529f7b396
Jan Štefl <janstefl.cz@gmail.cz>
2011-12-11 00:02:38 +0100

    Basic initialization added.

--- src/test/java/cz/janstefl/tasksync/core/TaskSyncTest.java
+++ src/test/java/cz/janstefl/tasksync/core/TaskSyncTest.java
@@ -1,11 +1,14 @@
 package cz.janstefl.tasksync.core;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
+import javax.naming.NamingException;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -24,6 +27,10 @@ public class TaskSyncTest {
     properties.put(EJBContainer.MODULES, new File("target/classes"));
     ec = EJBContainer.createEJBContainer(properties);
     ctx = ec.getContext();
+
+    generateUsers();
+    generateSystems();
+    generateConnections();
   }
 
   @AfterClass
@@ -33,13 +40,34 @@ public class TaskSyncTest {
   }
 
   @Test
-  public void testServlet() throws Exception {
-    TaskSync ts = (TaskSync) ctx.lookup(
-        "java:global/classes/TaskSync!cz.janstefl.tasksync.core.TaskSync");
+  public void betterTest() throws Exception {
+    TaskSync ts = (TaskSync) ctx
+        .lookup("java:global/classes/TaskSync!cz.janstefl.tasksync.core.TaskSync");
+  }
 
-    UserItem user = new UserItem();
-    user.setEmail("janstefl.cz@gmail.com");
-    user = ts.createUser(user);
+  private static void generateUsers() throws NamingException {
+    List<UserItem> users = new ArrayList<UserItem>();
+    users.add(new UserItem(1, "janstefl.cz@gmail.com"));
+    users.add(new UserItem(2, "petrova.aneta@gmail.com"));
+    users.add(new UserItem(3, "alois@manas.cz"));
+    users.add(new UserItem(4, "jakekoliv@jmeno.cz"));
+    users.add(new UserItem(5, "arjen@lucassen.net"));
+
+    TaskSync ts = (TaskSync) ctx
+        .lookup("java:global/classes/TaskSync!cz.janstefl.tasksync.core.TaskSync");
+
+    ts.createUser(users);
+  }
+
+  private static void generateSystems() throws NamingException {
+    Systems systems = (Systems) ctx
+        .lookup("java:global/classes/Systems!cz.janstefl.tasksync.core.Systems");
+  }
+
+  private static void generateConnections() throws NamingException {
+    Connections connections = (Connections) ctx
+        .lookup("java:global/classes/Connections!cz.janstefl.tasksync.core.Connections");
+
   }
 
 }
